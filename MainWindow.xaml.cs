7d8527fe220dd87abc5f0ea9f4f95b8ca9f8bf1e
Jens Eresund <jens.eresund@gmail.com>
2024-10-18 16:51:48 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Exercises_ITHS.NET;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+
+		InitializeComponent();
+		for (int i = 0; i*100 < Width; i++)
+		{
+			topRow.ColumnDefinitions.Add(
+				new ColumnDefinition()
+				{
+					Width = new(1, GridUnitType.Star),
+					MaxWidth = 100,
+					MinWidth = 100
+				}
+				);
+			var btn = new Button() { Content = "Button " + i, };
+			btn.Click += (_, _) => Button_Click(i);
+			topRow.Children.Add(btn);
+			Grid.SetColumn(btn, i);
+		}
+
+		gridRowSelector.SelectedValuePath = "Row";
+		gridRowSelector.DisplayMemberPath = "ContentType";
+		for (int i = 0; i < mainContent.RowDefinitions.Count; i++)
+		{
+			
+			gridRowSelector.Items.Add(new RowIdentifier(){Row = i, ContentType = mainContent.RowDefinitions[i].Name });
+		}
+		gridColSelector.SelectedValuePath = "Col";
+		gridColSelector.DisplayMemberPath = "Field";
+		for (int i = 0; i < mainContent.ColumnDefinitions.Count; i++)
+		{
+			gridColSelector.Items.Add(new ColumnIdentifier(){Col=i, Field=mainContent.ColumnDefinitions[i].Name });
+		}
+		//btnstack.Style = new(typeof (Button)) { Setters = { new Setter { Property = Button.BackgroundProperty, Value = Brushes.Red } } };
+		//asd1.Click += (_,_) =>  Button_Click(1);
+		//wasd.Click += (_,_) =>  Button_Click(2);
+		//asd2.Click += (_,_) =>  Button_Click(3);
+	}
+	public class RowIdentifier
+	{
+		public int Row { get; init; }
+		public required string ContentType { get; init; }
+	}
+	public class ColumnIdentifier
+	{
+		public int Col { get; init; }
+		public required string Field { get; init; }
+	}
+	private void Button_Click(int buttonNr)
+	{
+
+		switch (buttonNr)
+		{
+			//case 1:
+			//	textbox.Text = "Button 1 clicked";
+			//	break;
+				
+			//case 2:
+			//	textbox.Text = "Button 2 clicked";
+			//	break;
+				
+			//case 3:
+			//	textbox.Text = "Button 3 clicked";
+			//	break;
+
+			default:
+				break;
+		}
+	}
+
+	private void Button_Click_Create(object sender, RoutedEventArgs e)
+	{
+		var name = elementName.Text;
+		var type = elementType.Text;
+		var row = gridRowSelector.SelectedIndex;
+		var col = gridColSelector.SelectedIndex;
+
+		switch (type)
+		{
+			case "TextBlock":
+				var newText = new TextBlock() { Text = name, TextWrapping = TextWrapping.Wrap };
+				mainContent.Children.Add(newText);
+				Grid.SetRow(newText, row);
+				Grid.SetColumn(newText, col);
+				break;
+
+			case "Button":
+				var newButton = new Button() { Content = name };
+				mainContent.Children.Add(newButton);
+				Grid.SetRow(newButton, row);
+				Grid.SetColumn(newButton, col);
+				break;
+
+			case "DataGrid":
+				var newGrid = new DataGrid() { Name = name };
+				mainContent.Children.Add(newGrid);
+				Grid.SetRow(newGrid, row);
+				Grid.SetColumn(newGrid, col);
+				break;
+
+			case "TabPanel":
+				var newTabPanel = new TabControl() { Name = name };
+				mainContent.Children.Add(newTabPanel);
+				Grid.SetRow(newTabPanel, row);
+				Grid.SetColumn(newTabPanel, col);
+				break;
+
+			default:
+				break;
+		}
+
+	}
+
+	private void elementName_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		//var asd = new Binding();
+		//asd.Converter
+	}
+
+	private void elementName_GotFocus(object sender, RoutedEventArgs e)
+	{
+		txtNamePlaceholder.Visibility = Visibility.Hidden;
+	}
+
+	private void elementName_LostFocus(object sender, RoutedEventArgs e)
+	{
+		if (elementName.Text.Length == 0)
+		{
+			txtNamePlaceholder.Visibility = Visibility.Visible;
+		}
+		else
+		{
+			txtNamePlaceholder.Visibility = Visibility.Hidden;
+		}
+	}
+}
+
+public static class  WPFExtenstions
+{
+	public static void GridExt(this Grid g)
+	{
+
+	}
+
+}
---
+++ StudentRegister.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+// !!!!! Your custom class must not be a nested class. !!!!!
+
+namespace WPF_Exercises_ITHS.NET;
+/// <summary>
+/// Interaction logic for StudentRegister.xaml
+/// </summary>
+public partial class StudentRegister : UserControl
+{
+	public string ContentName { get; set; } = "Students";
+
+	[Bindable(true)]
+    public ObservableCollection<Student> Students { get; set; } = new();
+    public StudentRegister()
+	{
+		InitializeComponent();
+
+		Students.Add(new Student("John", "Doe", "asd.asd@aasd.asd"));
+		Students.Add(new Student("Jane", "Doe", "asd.asd@aasd.asd"));
+		Students.Add(new Student("John", "Smith", "asd.asd@aasd.asd"));
+	}
+
+	public void Button_Click(object sender, RoutedEventArgs e)
+	{
+		if(studentCreateForm.DataContext is Student s)
+			Students.Add(s);
+		
+		//studentList.GetBindingExpression(ListBox.ItemsSourceProperty).UpdateTarget();
+		studentCreateForm.DataContext = new StudentValidationRule();
+
+		if (sender is Button b)
+		{
+			b.GetBindingExpression(IsEnabledProperty).UpdateTarget();
+		}
+	}
+}
+
+public class Student
+{
+	public Student() { }
+	public Student(string firstname, string lastname, string email)
+	{
+		Firstname = firstname;
+		Lastname = lastname;
+		Email = email;
+	}
+
+	[Bindable(true)]
+	public string Firstname { get; 
+		set; }
+	[Bindable(true)]
+	public string Lastname { get; set; }
+	[Bindable(true)]
+	public string Email { get; set; }
+
+	public override string ToString()
+	{
+		return $"{Firstname} {Lastname}";
+	}
+}
+
+public class StudentValidationRule : ValidationRule, INotifyPropertyChanged
+{
+	private string _firstName = "";
+	private string _lastName = "";
+	private string _email = "";
+	private bool _isValid = false;
+	public string Firstname {
+		get => _firstName; 
+		set {
+			_firstName = value;
+			if (_firstName.Length > 0)
+				IsValid = true;
+		}
+	}
+	public string Lastname
+	{
+		get => _lastName;
+		set
+		{
+			_lastName = value;
+			if (_lastName.Length > 0)
+				IsValid = true;
+		}
+	}
+	public string Email
+	{
+		get => _email;
+		set
+		{
+			_email = value;
+			if(_email.Length > 0)
+				IsValid = true;
+		}
+	}
+    public bool IsValid
+	{
+		get => _firstName.Length > 0 && _lastName.Length > 0 && _email.Length > 0;
+		private set
+		{
+			if (_firstName.Length > 0 && _lastName.Length > 0 && _email.Length > 0)
+			{
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+	{
+		if (value is string str)
+		{
+			if (string.IsNullOrWhiteSpace(str))
+			{
+				return new ValidationResult(false, "Field cannot be empty.");
+			}
+			return ValidationResult.ValidResult;
+		}
+		return new ValidationResult(false, "Invalid input");
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
+
+
